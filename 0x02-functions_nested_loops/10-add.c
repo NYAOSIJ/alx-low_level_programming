72d712308a0b227c49d5436f21990365d6ef77af
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-21 15:49:45 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,16 @@
-nclude<stdio.h>
+ #include<stdio.h>
 /* User defined function sum that has two int
  * parameters. The function adds these numbers and
  * return the result of addition.
  */
-void add();                                               //Function Declaration
-int main()
+void add(); 
+int main ()                                            
 {
-  add();                                                  //Function Calling
+  add();                                                 
   return 0;
 }
 /* function definition*/
-void add()                                                //Function Definition
+void add()                                               
 {
    int a,b,c;
      c=a+b;
