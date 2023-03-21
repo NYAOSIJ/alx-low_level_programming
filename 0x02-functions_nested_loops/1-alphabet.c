3d4124c7e3c3b31978063812d29296a43c4c65ed
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-21 05:24:11 -0700

    alphate

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *void print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar ('\n');
+return (0);
+}
+
