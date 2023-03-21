0cd575c3e78bf415e3047a4fcf4c609b9e1a9fda
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-21 14:35:29 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,17 @@
-#include "main.h"
-#include <stdio.h>
-
+ #include "main.h"
 /**
-  * main - Entry point
+  * print_alphabet - Make the alphabet
   *
-  * Return: Always 0
+  * Return: void
   */
-int main(void)
+void print_alphabet(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+char c;
 
-return (0);
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+
+putchar ('\n');
 }
