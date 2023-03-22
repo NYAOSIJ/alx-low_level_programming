c53513f1e0efe2148720b02aa2a8671911ffacce
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 16:57:00 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,16 @@
 #include main.h
 /**
  * main - check the code
- *
+ *print_alphabet - prints the alphabet in lowercase, followed by a new line.
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+}
 int main(void)
 {
   print_alphabet();
