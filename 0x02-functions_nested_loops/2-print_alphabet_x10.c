77bdc75e5683c659fbfd85dd298eec8482a6496c
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 18:33:59 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,11 @@
 /**
  * print_alphabet_x10 - print a - z ten times
  */
-void print_alphabet_x10()
+void print_alphabet_x10();
 {
   int i;
   char j;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 10; i++);
   {
 for (j = 'a'; j <= 'z'; j++)
 {
@@ -15,3 +15,4 @@ putchar(j);
 putchar('\n');
 }
 }
+
