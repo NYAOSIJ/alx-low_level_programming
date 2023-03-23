3ea1de7545c937915f187b02ca0d44d5363f3860
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 17:22:51 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
- 
+ #include main.h
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
@@ -9,7 +9,7 @@ char letter;
 while (count++ <= 9)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n');
+putchar(letter);
+putchar('\n');
 }
 }
