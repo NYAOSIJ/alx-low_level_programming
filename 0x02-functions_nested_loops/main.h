d5de8b52cfe5bf3cd15de190315b75886d950dcf
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 17:35:46 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,19 +6,8 @@
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
-{
-int count = 0;
-char letter;
-while (count++ <= 9)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n');
-}
-}
 int main(void)
 {
     print_alphabet_x10();
     return (0);
 }
-
