c04a13c87388777ec657f18a6a18fc292710a729
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 18:16:38 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(i,j)
 int main(void)
 {
     print_alphabet_x10();
     return (0);
 }
+
