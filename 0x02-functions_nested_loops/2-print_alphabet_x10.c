ba4520df9fed38a26ec749789e78dd31f4ebb769
NYAOSIJ <seniornyaosi@gmail.com>
2023-03-22 18:08:38 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,11 @@
-#include "main.h"
-
+ #include "main.h"
 /**
  * print_alphabet_x10 - print a - z ten times
  */
-
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
   int i;
   char j;
-
   for (i = 0; i < 10; i++)
   {
 for (j = 'a'; j <= 'z'; j++)
@@ -16,5 +13,5 @@ for (j = 'a'; j <= 'z'; j++)
 putchar(j);
 }
 putchar('\n');
-  }
+}
 }
